0bfbc896c5e711779d91daf6b8ef07c336891ab9
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-05-17 01:14:52 +0530

    Added navigation menu with routing

--- ChallengeAppProject/challenge-app-ui/src/app/app-routing.module.ts
+++ ChallengeAppProject/challenge-app-ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ChallengeComponent} from "./challenge/challenge.component";
+import {SavedChallengesComponent} from "./saved-challenges/saved-challenges.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: ChallengeComponent},
+  {path: 'saved-challenges', component: SavedChallengesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

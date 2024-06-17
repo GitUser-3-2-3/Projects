3d22dc1737a9fc077ac2134ddf970df098a99b43
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-06-17 20:08:49 +0530

    initial commit

---
+++ Spring Security/role-based-access-control/src/main/java/com/parth/rbac/rolebasedaccesscontrol/RoleBasedAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.parth.rbac.rolebasedaccesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAccessControlApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(RoleBasedAccessControlApplication.class, args);
+   }
+
+}

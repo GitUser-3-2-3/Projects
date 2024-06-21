356ca58234b00dad7707d967ad13c59dc726266f
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-06-21 11:08:58 +0530

    added command line runner to initialize ADMIN in the database

--- Spring Security/role-based-access-control/src/main/java/com/parth/rbac/RoleBasedAccessControlApplication.java
+++ Spring Security/role-based-access-control/src/main/java/com/parth/rbac/RoleBasedAccessControlApplication.java
@@ -1,13 +1,29 @@
 package com.parth.rbac;
 
+import com.parth.rbac.model.Role;
+import com.parth.rbac.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RoleBasedAccessControlApplication {
 
    public static void main(String[] args) {
       SpringApplication.run(RoleBasedAccessControlApplication.class, args);
    }
 
+   @Bean
+   public CommandLineRunner runner(RoleRepository roleRepository) {
+      return args -> {
+         if (roleRepository.findByName("ADMIN").isEmpty()) {
+            roleRepository.save(
+                 Role.builder().name("ADMIN").build()
+            );
+         }
+      };
+   }
 }

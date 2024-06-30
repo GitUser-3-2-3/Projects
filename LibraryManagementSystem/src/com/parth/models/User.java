bddd34a33e9a7875bf6132f9d8fbbce57bc4734b
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-06-30 16:50:58 +0530

    class for user model and authentication

---
+++ LibraryManagementSystem/src/com/parth/models/User.java
@@ -0,0 +1,47 @@
+package com.parth.models;
+
+public class User {
+
+   private final String username;
+   private final String email;
+   private final String password;
+
+   public static class Builder {
+
+      private String username = null;
+      private final String email;
+      private final String password;
+
+      public Builder(String email, String password) {
+         this.email = email;
+         this.password = password;
+      }
+
+      public Builder username(String value) {
+         username = value;
+         return this;
+      }
+
+      public User build() {
+         return new User(this);
+      }
+   }
+
+   private User(Builder builder) {
+      this.username = builder.username;
+      this.email = builder.email;
+      this.password = builder.password;
+   }
+
+   public String getUsername() {
+      return username;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public boolean authenticate(String password) {
+      return this.password.equals(password);
+   }
+}
---
+++ LibraryManagementSystem/src/com/parth/security/AuthenticationSystem.java
@@ -0,0 +1,41 @@
+package com.parth.security;
+
+import com.parth.models.User;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AuthenticationSystem {
+
+   private final Map<String, User> users;
+
+   public AuthenticationSystem() {
+      users = new HashMap<>();
+      users.put("admin",
+           new User.Builder("parth@gmail.com", "pass1")
+                .username("parth").build()
+      );
+      users.put("user",
+           new User.Builder("juhi@gmail.com", "pass2")
+                .username("juhi").build()
+      );
+   }
+
+   public User login(Scanner sc) {
+      System.out.println("Enter your email: ");
+      String email = sc.nextLine();
+
+      System.out.println("Enter your password: ");
+      String password = sc.nextLine();
+
+      User user = users.get(email);
+      if (user != null && user.authenticate(password)) {
+         System.out.println("Login successful.");
+         return user;
+      } else {
+         System.out.println("Invalid username or password.");
+         return null;
+      }
+   }
+}

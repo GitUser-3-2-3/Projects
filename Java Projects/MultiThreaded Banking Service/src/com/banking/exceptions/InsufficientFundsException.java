be6777abf87d59adc618074a5237d3ab4b1650b3
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-08-11 10:56:58 +0530

    exception class when balance is insufficient

---
+++ Java Projects/MultiThreaded Banking Service/src/com/banking/exceptions/InsufficientFundsException.java
@@ -0,0 +1,33 @@
+package com.banking.exceptions;
+
+public class InsufficientFundsException extends Exception {
+
+    /**
+     * Constructs a new InsufficientFundsException with the specified detail message.
+     *
+     * @param message The detail message, saved for later retrieval by the getMessage() method.
+     */
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new InsufficientFundsException with the specified detail message and cause.
+     *
+     * @param message The detail message, saved for later retrieval by the getMessage() method.
+     * @param cause   The cause (which is saved for later retrieval by the getCause() method).
+     *                A null value is permitted, and indicates that the cause is nonexistent or unknown.
+     */
+    public InsufficientFundsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+
+
+
+
+
+
+
+

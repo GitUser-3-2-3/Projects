8b51bf5478544110b85f2a16ba49f9321e2bd83b
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-08-23 13:59:06 +0530

    nothing important

--- SimpleFileReader/src/main/java/com/project/simplereader/Main.java
+++ SimpleFileReader/src/main/java/com/project/simplereader/Main.java
@@ -2,6 +2,9 @@ package com.project.simplereader;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
+
+        Reader readerClass = new Reader();
+        readerClass.reader();
     }
 }

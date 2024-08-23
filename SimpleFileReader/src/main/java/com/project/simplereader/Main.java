e8547816dd980a1d5d41de49a82d9c4ba064fe3f
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-08-23 19:19:07 +0530

    ...

--- SimpleFileReader/src/main/java/com/project/simplereader/Main.java
+++ SimpleFileReader/src/main/java/com/project/simplereader/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println();
 
-        Reader readerClass = new Reader();
+        InputOutput readerClass = new InputOutput();
         readerClass.reader();
+        readerClass.writer();
     }
 }

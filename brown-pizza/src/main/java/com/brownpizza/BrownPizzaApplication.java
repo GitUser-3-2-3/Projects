bfd5c5e317e977c5ff04b49e41ce4b2aff4e40d1
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-08-27 19:54:43 +0530

    initial commit

---
+++ brown-pizza/src/main/java/com/brownpizza/BrownPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.brownpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrownPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrownPizzaApplication.class, args);
+    }
+
+}
---
+++ brown-pizza/src/test/java/com/brownpizza/BrownPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brownpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrownPizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

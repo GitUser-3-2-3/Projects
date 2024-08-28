42e713df3257271328a93efd5321cc970feebbad
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-08-28 21:13:40 +0530

    better price calculation

--- brown-pizza/src/main/java/com/brownpizza/util/PriceCalculator.java
+++ brown-pizza/src/main/java/com/brownpizza/util/PriceCalculator.java
@@ -2,7 +2,8 @@ package com.brownpizza.util;
 
 import com.brownpizza.model.Ingredient;
 import com.brownpizza.model.Pizza;
-import jakarta.validation.constraints.NotBlank;
+import com.brownpizza.model.Pizza.CrustType;
+import com.brownpizza.model.Pizza.PizzaSize;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
@@ -13,14 +14,23 @@ public class PriceCalculator {
 
     public static final BigDecimal PLATFORM_FEE_PERCENTAGE = new BigDecimal("0.05");
 
-    public static BigDecimal calculateBasePizzaPrice(@NotBlank String size) {
-        if ("Small".equalsIgnoreCase(size)) {
-            return new BigDecimal("8.99");
-        } else if ("Medium".equalsIgnoreCase(size)) {
-            return new BigDecimal("10.99");
-        } else {
-            return new BigDecimal("12.99");
-        }
+    public static BigDecimal calculateBasePizzaPrice(
+        @NotNull PizzaSize pizzaSize, @NotNull CrustType crustType
+    ) {
+        return switch (pizzaSize) {
+            case SMALL -> switch (crustType) {
+                case THIN_CRUST, HAND_TOSSED, PAN -> new BigDecimal("7.99");
+                default -> new BigDecimal("9.99");
+            };
+            case MEDIUM -> switch (crustType) {
+                case THIN_CRUST, HAND_TOSSED, PAN -> new BigDecimal("9.99");
+                default -> new BigDecimal("11.99");
+            };
+            case LARGE -> switch (crustType) {
+                case THIN_CRUST, HAND_TOSSED, PAN -> new BigDecimal("11.99");
+                default -> new BigDecimal("13.99");
+            };
+        };
     }
 
     public static BigDecimal calculateFinalPizzaPrice(

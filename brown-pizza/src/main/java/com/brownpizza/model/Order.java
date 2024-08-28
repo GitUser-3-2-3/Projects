a2bdbac44fa624f224f12a9275e8954c79025612
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-08-28 13:22:13 +0530

    added method to calculate prices and separated address

--- brown-pizza/src/main/java/com/brownpizza/model/Order.java
+++ brown-pizza/src/main/java/com/brownpizza/model/Order.java
@@ -3,6 +3,7 @@ package com.brownpizza.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,47 +23,47 @@ public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "Name cannot be blank")
     @Size(min = 3, message = "Name must be 3 char long")
     private String customerName;
 
-    @NotBlank(message = "Street cannot be blank")
-    private String deliveryStreet;
-
-    @NotBlank(message = "City cannot be blank")
-    private String deliveryCity;
-
-    @NotBlank(message = "State cannot be blank")
-    private String deliveryState;
-
-    @NotBlank(message = "Pin-code cannot be blank")
-    @Size(min = 6, max = 6, message = "Pin-code must be 6 char in length")
-    private String pinCode;
+    @Embedded
+    private Address deliveryAddress;
 
     @NotNull(message = "Date & Time cannot be null")
     private LocalDateTime placedAt = LocalDateTime.now();
 
     @NotNull(message = "Total pizza price cannot be null")
+    @Positive(message = "Total Pizza price cannot be negative")
     private BigDecimal sumTotalPizza;
 
-    //* add a method to make 5% of the order price the platform fee.
-
     @NotNull(message = "Platform fee cannot be null")
+    @Positive(message = "Platform fee cannot be negative")
     private BigDecimal platformFee;
 
-    //* add a method to make delivery prices dynamic based on relative distance.
-
     @NotNull(message = "Delivery fee cannot be null")
+    @Positive(message = "Deliver fee cannot be negative")
     private BigDecimal deliveryFee;
 
     @NotNull(message = "Total price cannot be null")
+    @Positive(message = "Total price cannot be negative")
     private BigDecimal totalPrice;
 
     @OneToMany(cascade = CascadeType.ALL)
     private List<Pizza> pizzas;
+
+    public void calculatePlatformFee() {
+        platformFee = sumTotalPizza.multiply(new BigDecimal("0.05"));
+    }
+
+    //* add a method to make delivery prices dynamic based on relative distance.
+
+    public void totalPrice() {
+        totalPrice = sumTotalPizza.add(platformFee).add(deliveryFee);
+    }
 }
 
 

72ba0b6a2ca81b91546e0e5e431b82d9c7b04843
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-08-31 15:21:16 +0530

    changes made to provide final price

--- brown-pizza/src/main/java/com/brownpizza/configuration/WebConfig.java
+++ brown-pizza/src/main/java/com/brownpizza/configuration/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/html/home.html");
+        registry.addViewController("/").setViewName("brown-pizza");
     }
 }

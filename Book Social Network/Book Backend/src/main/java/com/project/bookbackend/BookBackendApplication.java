3d3c1b393f31cc2a2195cfb6dffe6b6bd2bd07e8
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-09-22 02:13:20 +0530

    book backend initialization

---
+++ Book Social Network/Book Backend/src/main/java/com/project/bookbackend/BookBackendApplication.java
@@ -0,0 +1,12 @@
+package com.project.bookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBackendApplication.class, args);
+	}
+}

2f87e16224d35a3a7cc7f73945adae0a6b4d02d4
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-09-22 01:59:32 +0530

    removed deleted files

--- brown-pizza/src/main/java/com/brownpizza/repository/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.brownpizza.repository;
-
-import com.brownpizza.model.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<Ingredient, String> {
-}
--- brown-pizza/src/main/java/com/brownpizza/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.brownpizza.repository;
-
-import com.brownpizza.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}

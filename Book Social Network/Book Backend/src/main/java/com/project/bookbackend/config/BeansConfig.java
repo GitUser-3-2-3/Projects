a6b92ca25c13076fb57df2d81c9c8cd72a8650ad
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-10-22 13:37:13 +0530

    changed strength of encoding

--- Book Social Network/Book Backend/src/main/java/com/project/bookbackend/config/BeansConfig.java
+++ Book Social Network/Book Backend/src/main/java/com/project/bookbackend/config/BeansConfig.java
@@ -44,7 +44,7 @@ public class BeansConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(10);
     }
 }
 

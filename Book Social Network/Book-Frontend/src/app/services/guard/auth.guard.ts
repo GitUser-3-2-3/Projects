d24193307bf0cc2781f9cfc20361bfe1b2768f36
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-12-03 18:29:23 +0530

    auth guard added

--- Book Social Network/Book-Frontend/src/app/services/guard/auth.guard.ts
+++ Book Social Network/Book-Frontend/src/app/services/guard/auth.guard.ts
@@ -1,5 +1,16 @@
-import {CanActivateFn} from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {TokenService} from "../token/token.service";
 
 export const authGuard: CanActivateFn = () => {
+    const tokenService = inject(TokenService);
+    const router = inject(Router);
+
+    if (tokenService.isNotValid()) {
+        router.navigate(['login']).then(
+            _ => console.log('invalid token!')
+        )
+        return false;
+    }
     return true;
 };

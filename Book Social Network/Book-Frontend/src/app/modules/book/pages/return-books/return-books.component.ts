3426b684f9696d2415e483165a4dea9796189264
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-12-03 18:10:31 +0530

    approve return functionality implemented

--- Book Social Network/Book-Frontend/src/app/modules/book/pages/return-books/return-books.component.ts
+++ Book Social Network/Book-Frontend/src/app/modules/book/pages/return-books/return-books.component.ts
@@ -1,10 +1,99 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PageResponseBorrowedBookResponse} from "../../../../services/models/page-response-borrowed-book-response";
+import {BookService} from "../../../../services/services/book.service";
+import {BorrowedBookResponse} from "../../../../services/models/borrowed-book-response";
 
 @Component({
     selector: 'app-return-books',
     templateUrl: './return-books.component.html',
     styleUrls: ['./return-books.component.scss']
 })
-export class ReturnBooksComponent {
+export class ReturnBooksComponent implements OnInit {
 
+    returnedBooks: PageResponseBorrowedBookResponse = {};
+    level = 'success';
+    message = '';
+    page = 0;
+    size = 5;
+
+    constructor(private bookService: BookService) {
+    }
+
+    ngOnInit(): void {
+        this.findAllReturnedBooks();
+    }
+
+    private findAllReturnedBooks() {
+        this.bookService.findAllReturnedBooks({page: this.page, size: this.size}).subscribe({
+            next: (res) => {
+                this.returnedBooks = res;
+            }
+        })
+    }
+
+    goToFirstPage() {
+        this.page = 0;
+        this.findAllReturnedBooks();
+    }
+
+    goToPreviousPage() {
+        this.page = this.page - 1;
+        this.findAllReturnedBooks();
+    }
+
+    goToPage(index: number) {
+        this.page = index;
+        this.findAllReturnedBooks();
+    }
+
+    goToNextPage() {
+        this.page = this.page + 1;
+        this.findAllReturnedBooks();
+    }
+
+    goToLastPage() {
+        this.page = this.returnedBooks.totalPages as number - 1;
+        this.findAllReturnedBooks();
+    }
+
+    get isLastPage(): boolean {
+        return this.page == this.returnedBooks.totalPages as number - 1;
+    }
+
+    approveReturn(book: BorrowedBookResponse) {
+        if (!book.returned) {
+            this.level = 'error';
+            this.clearMessageAfterDelay();
+            this.message = "Book not yet returned!"
+            return;
+        }
+        if (book.returnApproved) {
+            this.level = 'warning';
+            this.clearMessageAfterDelay();
+            this.message = "Book return already approved!"
+        }
+        this.bookService.approveBookReturn({"book-id": book.id as number}).subscribe({
+            next: () => {
+                this.level = 'success';
+                this.findAllReturnedBooks();
+                this.clearMessageAfterDelay();
+                this.message = "Book return approved!"
+            }
+        })
+    }
+
+    private clearMessageAfterDelay() {
+        setTimeout(() => {
+            this.message = '';
+        }, 5000);
+    }
 }
+
+
+
+
+
+
+
+
+

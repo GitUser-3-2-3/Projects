b43d47a90c33140cce8aa98a1107402b61acec4a
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-12-03 17:06:33 +0530

    return book comp

---
+++ Book Social Network/Book-Frontend/src/app/modules/book/pages/return-books/return-books.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-return-books',
+    templateUrl: './return-books.component.html',
+    styleUrls: ['./return-books.component.scss']
+})
+export class ReturnBooksComponent {
+
+}

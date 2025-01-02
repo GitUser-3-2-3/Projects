e7db1371d0bdce35b40e878b6a168a21a3f0a5ce
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-01-02 13:22:13 +0530

    code cleanup

--- Book Social Network/Book Backend/src/main/java/com/project/bookbackend/book/BookMapper.java
+++ Book Social Network/Book Backend/src/main/java/com/project/bookbackend/book/BookMapper.java
@@ -9,29 +9,38 @@ public class BookMapper {
 
     public Book toBook(BookRequest req) {
 
-        return Book.builder().id(req.id()).title(req.title())
-            .authorName(req.authorName()).isbn(req.isbn())
-            .synopsis(req.synopsis()).isArchived(false)
+        return Book.builder().id(req.id())
+            .title(req.title())
+            .authorName(req.authorName())
+            .isbn(req.isbn())
+            .synopsis(req.synopsis())
+            .isArchived(false)
             .isShareable(req.shareable())
             .build();
     }
 
     public BookResponse toBookResponse(Book req) {
 
-        return BookResponse.builder().id(req.getId()).title(req.getTitle())
-            .authorName(req.getAuthorName()).isbn(req.getIsbn())
+        return BookResponse.builder().id(req.getId())
+            .title(req.getTitle())
+            .authorName(req.getAuthorName())
+            .isbn(req.getIsbn())
             .synopsis(req.getSynopsis())
-            .rating(req.getRating()).owner(req.getOwner().getFullName())
+            .rating(req.getRating())
+            .owner(req.getOwner().getFullName())
             .isArchived(req.isArchived())
-            .isShareable(req.isShareable()).bookCover(FileUtils.readCoverFromLocation(req.getBookCover()))
+            .isShareable(req.isShareable())
+            .bookCover(FileUtils.readCoverFromLocation(req.getBookCover()))
             .build();
     }
 
     public BorrowedBookResponse toBorrowedBookResponse(BookTransactionHistory req) {
         final Book book = req.getBook();
 
-        return BorrowedBookResponse.builder().id(book.getId()).title(book.getTitle())
-            .authorName(book.getAuthorName()).isbn(book.getIsbn())
+        return BorrowedBookResponse.builder().id(book.getId())
+            .title(book.getTitle())
+            .authorName(book.getAuthorName())
+            .isbn(book.getIsbn())
             .returned(req.isReturned())
             .returnApproved(req.isReturnApproved())
             .build();
